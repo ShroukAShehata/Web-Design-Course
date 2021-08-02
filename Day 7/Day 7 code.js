608b905193a9c127ddd22683950ad47bd8ce8f69
ShroukAShehata <47907240+ShroukAShehata@users.noreply.github.com>
2021-08-02 09:31:06 +0200

    Add files via upload

---
+++ Day 7/Day 7 code.js
@@ -0,0 +1,14 @@
+window.alert("Welcome");
+
+document.getElementById("e1").innerHTML = "Welcome to our school website";
+
+document.getElementById("e2").innerHTML = "WE School";
+
+function campname(){
+  document.getElementById("e4").innerHTML = "Future Career Camp";
+}
+
+
+function track(){
+  document.getElementById('e7').innerHTML = "Web Design";
+}
